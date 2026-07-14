fe8c84b34b8284f33dae64fc28fefb15dfd455f6
Srikar Chowdary <csnscsns682@gmail.com>
2026-07-14 19:43:20 +0530

    First Romantic Website ❤️

---
+++ script.js
@@ -0,0 +1,10 @@
+function showLove() {
+
+    const music = document.getElementById("bgMusic");
+
+    music.play();
+
+    document.getElementById("message").innerHTML =
+        "❤️ Every heartbeat of mine whispers only one name... <br><br> My Muddulaaa Gummaaa ❤️";
+
+}
